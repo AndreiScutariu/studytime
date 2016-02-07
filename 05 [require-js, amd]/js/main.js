c786fad7bfbe21ef925428a5021ffa9ebbc9b6d7
= <=>
2016-02-07 21:17:34 +0200

    com

--- 05 [require-js, amd]/js/main.js
+++ 05 [require-js, amd]/js/main.js
@@ -1,23 +1,32 @@
-function add() {
-    dataStore.save( {value: $("#value").val()} );
-    $("#value").val('');
-}
+var app = (function () {
+    function add() {
+        dataStore.save( {value: $("#value").val()} );
+        $("#value").val('');
+    }
 
-function load() {
-    var obj = dataStore.load();
-    alert(obj.value);
-}
+    function load() {
+        var obj = dataStore.load();
+        alert(obj.value);
+    }
 
-function clear() {
-    dataStore.clear();
-}
+    function clear() {
+        dataStore.clear();
+    }
 
-function registerEventHandlers() {
-	$("#add").on("click", add);
-    $("#load").on("click", load);
-    $("#clear").on("click", clear);
-}
+    function registerEventHandlers() {
+    	$("#add").on("click", add);
+        $("#load").on("click", load);
+        $("#clear").on("click", clear);
+    }
+
+    return {
+        init: function() {
+            registerEventHandlers();
+        }
+    }
+})();
+
 
 $(function () {
-	registerEventHandlers();
+    app.init();
 });
